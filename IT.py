8ede6df93a8578bbd9b224732cb4ff2ccc7df24f
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-08-20 02:17:32 +0530

    Update file via script

--- IT.py
+++ IT.py
@@ -635,7 +635,7 @@ AIAPIIntegration = {
 }
 
 CollectionFramework = {
-    "Java application me bahut large number of elements store karne hain. ArrayList aur LinkedList me se appropriate collection kaise choose karoge?": False,
+    "Java application me bahut large number of elements store karne hain. ArrayList aur LinkedList me se appropriate collection kaise choose karoge?": True,
     "Aapko unique elements maintain karne hain aur fast lookup bhi chahiye. Kaunsi collection use karoge aur kyun?": False,
     "Application me key-value data store karna hai aur fast lookup required hai. Kaunsi Map implementation choose karoge?": False,
     "Collection ko iterate karte waqt elements remove karne hain. ConcurrentModificationException se kaise bachoge?": False,
