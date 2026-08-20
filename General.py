0f8dcca058ca9443f4438a545bbfe454c1339cd1
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-08-20 18:39:18 +0530

    Update file via script

--- General.py
+++ General.py
@@ -108,7 +108,7 @@ War = {
 # 3. IndiaPak (50 items)
 IndiaPak = {
     'Wagah Border ki Beating Retreat ceremony iconic hai.': True,
-    'India-Pak border line ko Radcliffe Line kehte hain.': False,
+    'India-Pak border line ko Radcliffe Line kehte hain.': True,
     'Siachen Glacier duniya ka sabse uncha militarized zone hai.': False,
     '1971 war ke baad Shimla Agreement sign hua tha.': False,
     'Dono desho ki cultural roots aapas me judi hain.': False,
