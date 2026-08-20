1f257f8c149bdc2d8d7c19cb40204a01f52d9f22
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-08-20 15:23:47 +0530

    Update file via script

--- IT.py
+++ IT.py
@@ -478,7 +478,7 @@ AWS = {
 DSA = {
     "Aapke paas ek large array hai aur efficiently duplicate elements find karne hain. Kaunsi approach use karoge aur kyun?": True,
     "Ek string me first non-repeating character efficiently find karna hai. Algorithm kaise design karoge?": True,
-    "Large dataset me kisi element ko repeatedly search karna hai. Search operation ko optimize kaise karoge?": False,
+    "Large dataset me kisi element ko repeatedly search karna hai. Search operation ko optimize kaise karoge?": True,
     "Graph me shortest path find karna hai jahan edges ki weights different hain. Kaunsa algorithm use karoge aur kyun?": False,
     "Aapko continuously incoming elements me top K largest elements maintain karne hain. Kaunsi data structure use karoge?": False,
     "Two Pointers Technique: Sorted array me Two Sum problem O(N) time and O(1) space complexity me kaise solve karein?": False,
