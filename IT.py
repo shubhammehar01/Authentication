d7689859070ab5d672427315341a2750fa15ef20
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-08-22 21:07:05 +0530

    Update file via script

--- IT.py
+++ IT.py
@@ -1,6 +1,6 @@
 Java = {
     "Java application me memory usage continuously badh raha hai. Memory leak ko kaise identify aur fix karoge?": True,
-    "Java me multiple threads same shared resource ko access kar rahe hain. Thread safety kaise ensure karoge?": False,
+    "Java me multiple threads same shared resource ko access kar rahe hain. Thread safety kaise ensure karoge?": True,
     "Production me Java application ke response time suddenly slow ho gaye. Performance issue ko kaise diagnose karoge?": False,
     "Java application me NullPointerException frequently aa rahi hai. Root cause identify karke issue ko kaise prevent karoge?": False,
     "Large amount of data process karte waqt Java application OutOfMemoryError de rahi hai. Is problem ko kaise solve karoge?": False,
