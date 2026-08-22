eea3123cebab392f6b216b124db4a363868f67ee
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-08-22 10:55:34 +0530

    Update file via script

--- General.py
+++ General.py
@@ -55,7 +55,7 @@ AnimalPicture = {
 War = {
     'History ki sabse lambi jang sirf 38 minute chali thi.': True,
     'World War me pigeons ne secret messages deliver kiye the.': True,
-    'Purane zamane me war ke liye haathiyo ka use hota hai.': False,
+    'Purane zamane me war ke liye haathiyo ka use hota hai.': True,
     'Enigma machine ne World War 2 ka rukh badal diya tha.': False,
     'Purane kile aisi engineering se bante the jo todna namumkin tha.': False,
     'Spartan warriors bachpan se hi military training shuru kar dete the.': False,
