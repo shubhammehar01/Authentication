72221710808c01d99b985b9c682d332c000c067d
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-08-23 00:20:15 +0530

    Update file via script

--- IT.py
+++ IT.py
@@ -107,7 +107,7 @@ Python = {
 JavaScript = {
     "JavaScript application me API calls ke baad UI frequently freeze ho raha hai. Performance issue ko kaise diagnose karoge?": True,
     "Multiple asynchronous API calls ko parallel execute karke results efficiently combine karne hain. Kaise implement karoge?": True,
-    "JavaScript application me memory usage continuously increase ho raha hai. Memory leak ko kaise identify karoge?": False,
+    "JavaScript application me memory usage continuously increase ho raha hai. Memory leak ko kaise identify karoge?": True,
     "User rapidly button click kar raha hai aur multiple unnecessary API requests ja rahi hain. Is problem ko kaise solve karoge?": False,
     "Browser application me sensitive data ko securely store karna hai. Client-side storage ke liye kya approach use karoge?": False,
     "Event Loop, Call Stack, Microtask Queue aur Macrotask Queue internal execution flow browser me kaise work karta hai?": False,
