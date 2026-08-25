c51b014509f6c27af2d15f9c657b60af3b2b938c
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-08-25 07:17:28 +0530

    Update file via script

--- General.py
+++ General.py
@@ -58,7 +58,7 @@ War = {
     'Purane zamane me war ke liye haathiyo ka use hota hai.': True,
     'Enigma machine ne World War 2 ka rukh badal diya tha.': True,
     'Purane kile aisi engineering se bante the jo todna namumkin tha.': True,
-    'Spartan warriors bachpan se hi military training shuru kar dete the.': False,
+    'Spartan warriors bachpan se hi military training shuru kar dete the.': True,
     'Roman empire ke paas world ki sabse disciplined army thi.': False,
     'Trojan war me wooden horse ka use kiya gaya tha.': False,
     'Samurai warriors Japan me honor ke liye jeete the.': False,
