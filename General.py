a98c068de2ae7e480172b435e4c22911635f5f59
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-08-25 12:35:05 +0530

    Update file via script

--- General.py
+++ General.py
@@ -166,7 +166,7 @@ NaturalDisaster = {
     'Duniya ka sabse bada volcano Mauna Loa hai.': True,
     'Jungle ki aag garmi aur sadi patti se lagati hai.': True,
     'Richter scale par earthquake ki intensity measure hoti hai.': True,
-    'Hurricanes aur typhoons tropical oceans me bante hain.': False,
+    'Hurricanes aur typhoons tropical oceans me bante hain.': True,
     'Tornadoes ki wind speed 400 km/h tak ho sakti hai.': False,
     'Avalanche snow slopes par achanak slide hone se hota hai.': False,
     'Drought lambe samay tak barish na hone ki wajah se hota hai.': False,
