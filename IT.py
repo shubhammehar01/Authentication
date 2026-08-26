2f6bca28d9d4def5225368488f3fd7f7b8c8ede0
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-08-26 12:37:12 +0530

    Update file via script

--- IT.py
+++ IT.py
@@ -800,7 +800,7 @@ Redis = {
     "Aapka Redis cache bahut zyada memory consume kar raha hai. Memory usage kaise diagnose aur control karoge?": True,
     "Redis temporarily unavailable ho jaaye to application ko database par automatically fallback karna hai. Ye strategy kaise implement karoge?": True,
     "Redis Data Structures Selection: Strings, Hashes, Lists, Sets, Sorted Sets (ZSET), Bitmaps, HyperLogLogs, Geospatial?": True,
-    "Redis Persistence Mechanisms: RDB (Redis Database Snapshots) vs AOF (Append Only File) vs Hybrid Persistence?": False,
+    "Redis Persistence Mechanisms: RDB (Redis Database Snapshots) vs AOF (Append Only File) vs Hybrid Persistence?": True,
     "Redis Memory Eviction Policies: volatile-lru, allkeys-lru, volatile-lfu, allkeys-lfu, volatile-ttl, noeviction?": False,
     "Redis Cache Stampede / Thundering Herd Problem Mitigation: Probabilistic Early Expiration (XFetch), Mutex Locking?": False,
     "Redis Sentinel vs Redis Cluster: High Availability Failover vs Automatic Sharding Partitioning setup?": False,
