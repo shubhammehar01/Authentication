92b01594a9abf68d8bfb2b438ec15b970271ff1c
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-09-02 16:44:20 +0530

    Update file via script

--- IT.py
+++ IT.py
@@ -906,7 +906,7 @@ MicroservicesArchitecture = {
     "Production me ek request multiple microservices se hokar jaati hai aur failure ka root cause identify karna difficult hai. Observability kaise implement karoge?": True,
     "Microservices ki number rapidly increase ho rahi hai aur service discovery manage karna difficult ho raha hai. Architecture ko kaise design karoge?": True,
     "Monolithic to Microservices Migration Strategies: Strangler Fig Pattern, Domain-Driven Design Bounded Contexts?": True,
-    "Service Discovery Pattern: Client-Side Discovery vs Server-Side Discovery (Eureka, Consul, Kubernetes DNS)?": False,
+    "Service Discovery Pattern: Client-Side Discovery vs Server-Side Discovery (Eureka, Consul, Kubernetes DNS)?": True,
     "API Gateway Pattern: Centralized Routing, Rate Limiting, Authentication, Protocol Translation (BFF Pattern)?": False,
     "Inter-Service Communication: Synchronous REST/gRPC vs Asynchronous Message Driven (Kafka/RabbitMQ) Event-Driven?": False,
     "Resilience Patterns: Circuit Breaker, Bulkhead Isolation, Retry with Exponential Backoff, Rate Limiter (Resilience4j)?": False,
