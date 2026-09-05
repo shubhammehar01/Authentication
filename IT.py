1cf2011be0af57bba97506c0bdda19350553a3c1
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-09-05 20:41:01 +0530

    Update file via script

--- IT.py
+++ IT.py
@@ -749,7 +749,7 @@ Git = {
     "Git Merge vs Git Rebase: Linear Commit History vs Preservation of Merge Commits trade-offs?": True,
     "Git Reset (Soft vs Mixed vs Hard Reset) Staging Area and Working Directory state alterations?": True,
     "Git Revert vs Git Reset: Public Published Branches commit undo safely without history rewriting?": True,
-    "Git Cherry-Pick picking specific hotfix commits production branch release coordination?": False,
+    "Git Cherry-Pick picking specific hotfix commits production branch release coordination?": True,
     "Git Interactive Rebase (`git rebase -i`) squashing, rewording, dropping, reordering commits history cleanup?": False,
     "Git Stash management (`git stash push -m`, `pop`, `apply`, `drop`, `branch`) context switching?": False,
     "Git Bisect binary search debugging tracking breaking bug introduction commit history?": False,
