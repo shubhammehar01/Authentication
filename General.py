9ca8c95cae750ad0ffb9af9b32be2c667879b1c9
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-09-06 09:50:03 +0530

    Update file via script

--- General.py
+++ General.py
@@ -442,7 +442,7 @@ GandhiMarch = {
     '24 days tak ye yatra chali thi.': True, # Wait, keep regular syntax:
     '24 days tak ye yatra chali thi.': True,
     '6 April 1930 ko namak todkar kanoon todha gaya tha.': True,
-    'Civil Disobedience Movement ki shuruwat isse hui thi.': False,
+    'Civil Disobedience Movement ki shuruwat isse hui thi.': True,
     'International media ne is event ko widely cover kiya tha.': False,
     'Sarojini Naidu bhi is movement ki prominent leader thin.': False,
     'Dharasana salt works protest bhi iske baad hua tha.': False,
