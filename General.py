e2e246408495a5a38e888b8488bbed2ba10e9623
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-09-08 16:40:05 +0530

    Update file via script

--- General.py
+++ General.py
@@ -279,7 +279,7 @@ factoryEngineers = {
     'Quality control sensors defective products detect karte hain.': True,
     'Lean manufacturing waste reduce karne par focus karti hai.': True,
     'Six Sigma methodology process improvement ke liye hoti hai.': True,
-    'Preventive maintenance machines breakdown hone se bachati hai.': False,
+    'Preventive maintenance machines breakdown hone se bachati hai.': True,
     'CAD software se parts ki 3D modeling ki jaati hai.': False,
     'PLC (Programmable Logic Controller) factory machines control karta hai.': False,
     'Conveyor belts materials ek jagah se dusri jagah move karti hain.': False,
