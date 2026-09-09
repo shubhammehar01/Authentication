4952ec8011e36a07568961a074523c3ae41a7074
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-09-09 10:01:34 +0530

    Update file via script

--- IT.py
+++ IT.py
@@ -851,7 +851,7 @@ RestAPIDesign = {
     "REST API me client ko sirf required fields return karni hain taaki response size kam ho. Isko kaise design karoge?": True,
     "API me ek request successfully process ho gayi lekin client ko response nahi mila aur client ne request retry kar di. Duplicate operation kaise prevent karoge?": True,
     "REST API me breaking changes introduce karni hain lekin existing clients bhi support karne hain. API versioning kaise handle karoge?": True,
-    "REST API par unauthorized aur excessive requests aa rahi hain. Authentication, authorization aur rate limiting kaise design karoge?": False,
+    "REST API par unauthorized aur excessive requests aa rahi hain. Authentication, authorization aur rate limiting kaise design karoge?": True,
     "REST Architectural Constraints: Client-Server, Stateless, Cacheable, Layered System, Code on Demand, Uniform Interface?": False,
     "HTTP Methods Semantics and Idempotency: GET, POST, PUT, DELETE, PATCH, OPTIONS, HEAD safe vs unsafe methods?": False,
     "HTTP Status Codes Proper Selection: 2xx Success, 3xx Redirection, 4xx Client Errors, 5xx Server Errors categorization?": False,
