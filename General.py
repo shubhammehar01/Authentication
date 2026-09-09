fa182051d9876ab7fdebea3bc46a9f16ba9f6f85
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-09-09 16:45:45 +0530

    Update file via script

--- General.py
+++ General.py
@@ -119,7 +119,7 @@ IndiaPak = {
     'Kartarpur Corridor pilgrims ke liye bina visa visit allow karta hai.': True,
     '1965 ki war kutch aur kashmir border par ladhi gayi thi.': True,
     'Kargil conflict 1999 me high altitude par hua tha.': True,
-    'LOC (Line of Control)ashmir region ko divide karti hai.': False,
+    'LOC (Line of Control)ashmir region ko divide karti hai.': True,
     'Sufi music aur qawwali dono desho me common heritage hain.': False,
     'Bollywood movies aur music dono taraf pasand kiye jaate hain.': False,
     'Tashkent Declaration 1966 me sign hua tha.': False,
