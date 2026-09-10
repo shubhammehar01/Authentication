15d036c377f431eb2f019e4a444337b0260882af
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-09-10 09:53:08 +0530

    Update file via script

--- General.py
+++ General.py
@@ -172,7 +172,7 @@ NaturalDisaster = {
     'Drought lambe samay tak barish na hone ki wajah se hota hai.': True,
     'Landslide heavy rain ya earthquake se hill slopes par hota hai.': True,
     'Volcanic ash aviation engine ke liye dangerous hoti hai.': True,
-    'Earthquake ke waves ko seismograph se record karte hain.': False,
+    'Earthquake ke waves ko seismograph se record karte hain.': True,
     'Flood rivers overflow hone ya cloudburst se aati hai.': False,
     'Wildfires ko control karna bahut difficult hota hai.': False,
     'Lightning strike se kai baar bade forest fires shuru hote hain.': False,
