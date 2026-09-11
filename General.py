35aab64f6bc844e4969545c04ddeb423ae8c8701
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-09-12 04:46:00 +0530

    Update file via script

--- General.py
+++ General.py
@@ -65,7 +65,7 @@ War = {
     'World War 1 me tank sabse pehle use kiye gaye the.': True,
     'Cold War me direct fighting nahi hui balki arms race thi.': True,
     'Guerrilla warfare tactics me chhupkar hamla kiya jata hai.': True,
-    'Naval wars me submarines game changer sabit hui hain.': False,
+    'Naval wars me submarines game changer sabit hui hain.': True,
     'Cyber warfare aaj ke samay me sabse bada threat ban gaya hai.': False,
     'Drones ka use modern wars me tezi se badh gaya hai.': False,
     'Geneva Convention war prisoners ke rights protect karta hai.': False,
