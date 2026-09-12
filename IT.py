913d0e4c891a58ebb48789e09fb30cb3af94faa5
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-09-13 02:06:48 +0530

    Update file via script

--- IT.py
+++ IT.py
@@ -803,7 +803,7 @@ Redis = {
     "Redis Persistence Mechanisms: RDB (Redis Database Snapshots) vs AOF (Append Only File) vs Hybrid Persistence?": True,
     "Redis Memory Eviction Policies: volatile-lru, allkeys-lru, volatile-lfu, allkeys-lfu, volatile-ttl, noeviction?": True,
     "Redis Cache Stampede / Thundering Herd Problem Mitigation: Probabilistic Early Expiration (XFetch), Mutex Locking?": True,
-    "Redis Sentinel vs Redis Cluster: High Availability Failover vs Automatic Sharding Partitioning setup?": False,
+    "Redis Sentinel vs Redis Cluster: High Availability Failover vs Automatic Sharding Partitioning setup?": True,
     "Redis Pub/Sub vs Redis Streams: Fire-and-forget messaging vs Persistent Consumer Groups Queue processing?": False,
     "Redis Key Expiration Implementation: Passive vs Active Expiration TTL Memory Reclamation Mechanics?": False,
     "Redis Transactions (MULTI, EXEC, WATCH, DISCARD) Optimistic Locking CAS implementation?": False,
