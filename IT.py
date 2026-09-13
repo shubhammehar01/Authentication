ee82fa3321fcc730ee1d33d06b873b7a4ba4d31c
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-09-14 02:20:42 +0530

    Update file via script

--- IT.py
+++ IT.py
@@ -644,7 +644,7 @@ CollectionFramework = {
     "ConcurrentHashMap vs SynchronizedMap vs Hashtable: Segment Locking vs Bucket Locking concurrency performance?": True,
     "ArrayList Dynamic Resizing Factor (50% capacity growth) array copy performance overhead mitigation?": True,
     "LinkedHashMap Access-Order vs Insertion-Order configuration LRU Cache implementation design?": True,
-    "TreeMap / TreeSet Red-Black Tree Implementation Comparator vs Comparable custom sorting?": False,
+    "TreeMap / TreeSet Red-Black Tree Implementation Comparator vs Comparable custom sorting?": True,
     "CopyOnWriteArrayList / CopyOnWriteArraySet read-heavy write-rare thread-safe iterator mechanics?": False,
     "ArrayDeque vs Stack / LinkedList: Double-Ended Queue performance overhead memory locality benchmark?": False,
     "PriorityQueue Internal Min/Max Binary Heap Structure siftUp / siftDown log(N) insertion deletion?": False,
