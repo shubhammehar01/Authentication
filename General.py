2319d36dd8ac38ae156abcb3a3c833078aa94eb2
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-09-14 18:06:02 +0530

    Update file via script

--- General.py
+++ General.py
@@ -282,7 +282,7 @@ factoryEngineers = {
     'Preventive maintenance machines breakdown hone se bachati hai.': True,
     'CAD software se parts ki 3D modeling ki jaati hai.': True,
     'PLC (Programmable Logic Controller) factory machines control karta hai.': True,
-    'Conveyor belts materials ek jagah se dusri jagah move karti hain.': False,
+    'Conveyor belts materials ek jagah se dusri jagah move karti hain.': True,
     'Supply chain management raw material timing manage karta hai.': False,
     'Industrial IoT (IIoT) real-time data monitoring deta hai.': False,
     'Assembly line Henry Ford ne popular ki thi.': False,
