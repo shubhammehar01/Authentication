b9498b4f4c5cac8381f92af1ed4fd3b2459d0fce
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-09-16 22:16:48 +0530

    Update file via script

--- General.py
+++ General.py
@@ -497,7 +497,7 @@ PoliceSearching = {
     'CSI teams crime scene se evidence collect karti hain.': True,
     'Missing person search operations me drones use hote hain.': True,
     'Highway checkposts pe vehicles ki checking hoti hai.': True,
-    'Interpol international police cooperation network hai.': False,
+    'Interpol international police cooperation network hai.': True,
     'Undercover operations me police secret agent bhejti hai.': False,
     'Raid actions me illegal substances ya criminals pakde jaate hain.': False,
     'APIS system automated fingerprint identification karta hai.': False,
