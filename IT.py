cf8a44f80b926f38f1bda17e1bf8697c2c6269a2
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-09-16 16:58:51 +0530

    Update file via script

--- IT.py
+++ IT.py
@@ -117,7 +117,7 @@ JavaScript = {
     "Promise.all vs Promise.allSettled vs Promise.race vs Promise.any: Concurrent asynchronous operational handling patterns?": True,
     "Web Workers use karke heavy computational JavaScript execution UI thread ko unblock rakhne me kaise leverage karein?": True,
     "DOM Manipulation batching aur Virtual DOM reconciliation repaint aur reflow browser operational costs ko kaise kam karta hai?": True,
-    "Service Workers registration, caching strategies (Cache-First, Network-First) progressive offline web apps me kaise setup karein?": False,
+    "Service Workers registration, caching strategies (Cache-First, Network-First) progressive offline web apps me kaise setup karein?": True,
     "Cross-Origin Resource Sharing (CORS) preflight OPTIONS requests aur security headers browser level enforcement me kaise resolve karein?": False,
     "JavaScript Garbage Collector (Mark-and-Sweep) detached DOM nodes memory cleanup kaise karta hai?": False,
     "Strict Mode ('use strict') runtime scope enforcement JavaScript error traps silences ko noisy runtime exceptions me kaise badalta hai?": False,
