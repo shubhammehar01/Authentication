6236721ae26b1fd2d28cc8a1893f0f8a9aa14cd3
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-09-16 10:09:12 +0530

    Update file via script

--- General.py
+++ General.py
@@ -15,7 +15,7 @@ AnimalPicture = {
     'Tiger ki skin par bhi dhariyan hoti hain agar baal hata diye jayein.': True,
     'Penguin khara pani pee sakte hain kyunki unke paas salt gland hota hai.': True,
     'Owl 360 degree tak apna sir guma sakte hain.': True,
-    'Bees ki 5 aankhe hoti hain.': False,
+    'Bees ki 5 aankhe hoti hain.': True,
     'Horse khade-khade so sakte hain.': False,
     'Flamingo pink isliye hote hain kyunki wo shrimp khate hain.': False,
     'Snail 3 saal tak so sakta hai.': False,
