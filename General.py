c930337906e892d3c18ddbfd154dd310ec1336e0
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-09-20 02:03:43 +0530

    Update file via script

--- General.py
+++ General.py
@@ -229,7 +229,7 @@ SeaCreature = {
     'Sea turtle hazaron miles travel karke wapas nesting beach aate hain.': True,
     'Crab side ways move karte hain.': True,
     'Starfish apna lost arm dobara grow kar sakti hai.': True,
-    'Killer whales sebenarnya dolphins ki family se belong karti hain.': False,
+    'Killer whales sebenarnya dolphins ki family se belong karti hain.': True,
     'Anglerfish ke paas glowing lure hota hai shikaar ke liye.': False,
     'Pufferfish danger me aane par phool jaati hai aur poison release karti hai.': False,
     'Stingray ke tail par venomous barb hoti hai.': False,
