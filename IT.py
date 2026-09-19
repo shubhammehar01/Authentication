c132115b9530f0363a1070aab2b5795bb6146080
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-09-19 16:25:41 +0530

    Update file via script

--- IT.py
+++ IT.py
@@ -911,7 +911,7 @@ MicroservicesArchitecture = {
     "Inter-Service Communication: Synchronous REST/gRPC vs Asynchronous Message Driven (Kafka/RabbitMQ) Event-Driven?": True,
     "Resilience Patterns: Circuit Breaker, Bulkhead Isolation, Retry with Exponential Backoff, Rate Limiter (Resilience4j)?": True,
     "Distributed Transactions: Saga Pattern (Choreography-based vs Orchestration-based) Compensation Transactions?": True,
-    "Two-Phase Commit (2PC) Protocol limitations in Microservices Coordinator Failures Blocking Bottlenecks?": False,
+    "Two-Phase Commit (2PC) Protocol limitations in Microservices Coordinator Failures Blocking Bottlenecks?": True,
     "Eventual Consistency Data Consistency across Microservices Bounded Contexts Outbox Pattern implementation?": False,
     "Transactional Outbox Pattern & Change Data Capture (CDC - Debezium) Guaranteed Event Publishing?": False,
     "CQRS (Command Query Responsibility Segregation) Pattern Read Model Write Model Database Separation?": False,
