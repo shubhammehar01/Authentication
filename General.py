1d9b98d1f08eecc2c0bb26eea655f08db97ae8e8
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-09-22 01:34:46 +0530

    Update file via script

--- General.py
+++ General.py
@@ -127,7 +127,7 @@ IndiaPak = {
     'Shalimar Bagh Mughal architecture ka symbol hai.': True,
     'Biryani aur Chai dono cultures ka favourite part hai.': True,
     'Ghulam Ali aur Nusrat Fateh Ali Khan ke fans dono taraf hain.': True,
-    'Cross-border trade ke liye kuch checkposts banaye gaye hain.': False,
+    'Cross-border trade ke liye kuch checkposts banaye gaye hain.': True,
     '1983 aur 2011 World Cups me cricket rivalry dekhne ko mili.': False,
     'Gujral Doctrine India ki neighbourhood policy ka part raha hai.': False,
     'Maritime boundary Sir Creek area me dispute ka mudda hai.': False,
