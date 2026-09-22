c4f9053f4a9590edd7ae4d99313c4d3ce697fd9f
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-09-22 22:32:19 +0530

    Update file via script

--- General.py
+++ General.py
@@ -175,7 +175,7 @@ NaturalDisaster = {
     'Earthquake ke waves ko seismograph se record karte hain.': True,
     'Flood rivers overflow hone ya cloudburst se aati hai.': True,
     'Wildfires ko control karna bahut difficult hota hai.': True,
-    'Lightning strike se kai baar bade forest fires shuru hote hain.': False,
+    'Lightning strike se kai baar bade forest fires shuru hote hain.': True,
     'Sinkholes zameen ke andar cave collapse hone se bante hain.': False,
     'Blizzard severe snowstorm ko kehte hain jisme visibility low hoti hai.': False,
     'Heatwave extreme temperature increase hone se hoti hai.': False,
