6d4bd9a4c942b2a19df91cad7b60bbf68d99cc1a
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-09-26 10:15:11 +0530

    Update file via script

--- General.py
+++ General.py
@@ -129,7 +129,7 @@ IndiaPak = {
     'Ghulam Ali aur Nusrat Fateh Ali Khan ke fans dono taraf hain.': True,
     'Cross-border trade ke liye kuch checkposts banaye gaye hain.': True,
     '1983 aur 2011 World Cups me cricket rivalry dekhne ko mili.': True,
-    'Gujral Doctrine India ki neighbourhood policy ka part raha hai.': False,
+    'Gujral Doctrine India ki neighbourhood policy ka part raha hai.': True,
     'Maritime boundary Sir Creek area me dispute ka mudda hai.': False,
     'Cultural exchange programs history me promote kiye gaye hain.': False,
     'Classical dance aur music forms me similarities hain.': False,
