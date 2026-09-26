5c884d8e94a13628b43f680921798eae7a3dfec7
Shubham Mehar <91693626+shubhammehar01@users.noreply.github.com>
2026-09-26 16:53:44 +0530

    Update file via script

--- IT.py
+++ IT.py
@@ -854,7 +854,7 @@ RestAPIDesign = {
     "REST API par unauthorized aur excessive requests aa rahi hain. Authentication, authorization aur rate limiting kaise design karoge?": True,
     "REST Architectural Constraints: Client-Server, Stateless, Cacheable, Layered System, Code on Demand, Uniform Interface?": True,
     "HTTP Methods Semantics and Idempotency: GET, POST, PUT, DELETE, PATCH, OPTIONS, HEAD safe vs unsafe methods?": True,
-    "HTTP Status Codes Proper Selection: 2xx Success, 3xx Redirection, 4xx Client Errors, 5xx Server Errors categorization?": False,
+    "HTTP Status Codes Proper Selection: 2xx Success, 3xx Redirection, 4xx Client Errors, 5xx Server Errors categorization?": True,
     "REST API Pagination Strategies: Offset-Based vs Cursor-Based (Keyset) Pagination large dataset performance?": False,
     "REST API Filtering, Sorting, Searching Parameter Naming Standardizations Specification Guidelines?": False,
     "Idempotency Key Implementation (`Idempotency-Key` Header) Distributed Locks Redis Request Deduplication?": False,
